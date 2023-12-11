854d5600f616451a056f1b50e1a161b24d000b5c
Dafner Lorente <131700551+dafnerlorente@users.noreply.github.com>
2023-12-11 09:28:54 -0300

    geradorsenhas
    atualização de linhas de código

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 let SliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
+
 let sizePassword = document.querySelector("#valor");
-let Password = document.querySelector("#password");
+let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
 let novaSenha = "";
 
-sizePassword.innerHTML = sliderElement.value;
+sizePassword.innerHTML = SliderElement.value;
 
 slider.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
 
 function generatePassword(){
+
     let pass = "";
+
     for(let i = 0, n = charset.length; i < SliderElement.value; ++i){
+        
       pass += charset.charAt(Math.floor(Math.random() * n));
     }
 
     containerPassword.classList.remove("hide")
-    Password.innerHTML = pass;
+    password.innerHTML = pass;
 
 }
 
